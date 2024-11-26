d2b403803c9120492c51769f9abbca6a8558d378
Sebby75 <79416415+Sebb955@users.noreply.github.com>
2024-11-26 14:14:41 +0100

    feat : add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/tw-elements-react/dist/js/**/*.js", // Ajout pour tw-elements-react
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBackground: '#ffffff',
+        darkBackground: '#1e293b',
+        lightText: '#1f2937',
+        darkText: '#f8fafc',
+        darkHeader: '#0f172a',
+        primary: {
+          DEFAULT: '#6366f1',
+          600: '#4f46e5',
+          700: '#4338ca',
+        },
+        info: {
+          DEFAULT: '#54b4d3',
+          600: '#3b82f6',
+          700: '#2563eb',
+        },
+      },
+    },
   },
   plugins: [
     require('tw-elements-react/dist/plugin') // Ajout du plugin tw-elements-react
