cb99d45a5e830b1c438c39f6e4db3a361081d7d9
Sebby75 <79416415+Sebb955@users.noreply.github.com>
2025-01-02 15:58:41 +0100

    fix: fixes routing issues

--- src/presentation/components/common/Sidebar.tsx
+++ src/presentation/components/common/Sidebar.tsx
@@ -30,7 +30,7 @@ const sidebarItems: SidebarItemProps[] = [
     text: "Trade",
     alert: true,
     active: false,
-    link: "/",
+    link: "/dashboard",
   },
   {
     icon: <Book size={20} />,
